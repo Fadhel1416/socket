328fd9c54c20501708d3debb99fdd4ac217fc8b3
fadhelali1996 <alifadhel619@gmail.com>
2022-05-10 00:24:21 +0100

    first commit

---
+++ testoutput.php
@@ -0,0 +1,182 @@
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
+
+Notice: Trying to get property 'chat_user' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Notice: Trying to get property 'chat_message' of non-object in C:\wamp64\www\socket\php-socket.php on line 40
+
+Warning: socket_recv(): unable to read from socket [10053]: Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte.
+ in C:\wamp64\www\socket\php-socket.php on line 36
